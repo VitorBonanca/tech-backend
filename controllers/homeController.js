f507eb59e3cef42e427d95bed0d73672019e53e5
VitorBonanca <vitor.bonanca@protonmail.com>
2023-05-22 22:12:45 +0100

    added more attributes to the house model

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,25 +3,30 @@ const User = require("../models/user");
 const mongoose = require("mongoose");
 
 const addHome = async (req, res) => {
-  const { type } = req.body;
-  try {
-    const home = new Home({
-      type,
-      user: req.user._id
-    });
+  const { type, rate } = req.body;
+  if (!type || !rate) {
+    console.log("Fill empty fields");
+  } else {
+    try {
+      const home = new Home({
+        type,
+        rate,
+        user: req.user._id
+      });
 
-    const savedHome = await home.save();
+      const savedHome = await home.save();
 
-    const user = await User.updateOne({
-      _id: req.user._id,
-    }, {
-      $push: { homes:savedHome._id }
-    })
-   
-    res.redirect("/dashboard");
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Ocorreu um erro ao adicionar a casa.");
+      const user = await User.updateOne({
+        _id: req.user._id,
+      }, {
+        $push: { homes:savedHome._id }
+      })
+    
+      res.redirect("/dashboard");
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Ocorreu um erro ao adicionar a casa.");
+    }
   }
 };
 
--- models/home.js
+++ models/home.js
@@ -5,6 +5,18 @@ const homeSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  rate: {
+    type: Number,
+    required: true,
+  },
+  consumption: {
+    type: Number,
+    default: 0,
+  },
+  monthlyCost: {
+    type: Number,
+    default: 0.00,
+  },
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
