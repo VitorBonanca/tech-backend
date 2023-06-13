351798addd1c8fa6048aa8098dfcbc2a8179c49c
VitorBonanca <vitor.bonanca@protonmail.com>
2023-06-13 22:39:15 +0100

    added house total card

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,13 +1,25 @@
 const Home = require("../models/home");
+const HomeCalculator = require("../models/homeCalculator");
 
-//For Register Page
 const dashboardView = async (req, res) => {
+
   try {
     const homes = await Home.find({ user: req.user._id });
+
+    const totalHomes = homes.length;
+
+    // for (const home of homes) {
+    //   home.consumption = HomeCalculator.calculateConsumption(home);
+    //   home.monthlyCost = HomeCalculator.calculateMonthlyCost(home, rate);
+
+    // }
+
     res.render("dashboard", {
         user: req.user,
-        homes
+        homes,
+        totalHomes
     });
+
   } catch (error) {
     console.log(error);
     res.status(500).send("Ocorreu um erro ao exibir o painel de controle.");
